3a5d9e91fd5132b9783fdd730c6721fb757767fb
Martin Frank <martin.frank.ts@gmail.com>
2014-04-07 16:53:00 +0200

    create new rails app reviser without tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reviser::Application.config.secret_key_base = '28cff5a0e4fb6f66f4fda5ddfe6d14a794305a65cfe88121a11de44bc5b055d452212132c6e91af2b077e2be9b62567e3d811a1085ac0db012f1df67a006666f'
